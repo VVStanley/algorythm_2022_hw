997d9aacd97fe2de1067123cb8c5fab85d4a6401
VVStanley <stanley_23@mail.ru>
2023-02-15 09:37:58 +0500

    ДЗ9 линейная сортировка

--- utils/arr.py
+++ utils/arr.py
@@ -1,7 +1,11 @@
-from random import choices
+from random import choices, randrange
 from typing import List
 
 
 def get_random_arr(n: int) -> List[int]:
     sort_arr = [i for i in range(n)]
     return choices(sort_arr, k=len(sort_arr))
+
+
+def get_random_arr_three_digits(n: int) -> List[int]:
+    return [randrange(99, 1000) for i in range(n)]
